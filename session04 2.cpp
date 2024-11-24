d81ba3fc9f092d0c791938ae9343f4d2697ca4ed
tranxuanbach0 <trantiger706@gmail.com>
2024-11-24 14:32:23 +0700

    session04 2

---
+++ session04 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Nhap vao mot so nguyen: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d la so chan.\n", number);
+    } else {
+        printf("%d la so le.\n", number);
+    }
+
+    return 0;
+}
+
