95818f0e031db1313730282965e01d692f42984c
tranxuanbach0 <trantiger706@gmail.com>
2024-11-24 14:44:40 +0700

    session04 4

---
+++ session04 4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Nhap vao so thang (1-12): ");
+    scanf("%d", &month);
+
+    if (month < 1 || month > 12) {
+        printf("So thang khong hop le.\n");
+    } else {
+        if (month == 2) {
+            printf("Thang 2 co 28 hoac 29 ngay.\n");
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            printf("Thang %d co 30 ngay.\n", month);
+        } else {
+            printf("Thang %d co 31 ngay.\n", month);
+        }
+    }
+
+    return 0;
+}
+
